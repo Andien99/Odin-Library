3ab920d357d2266b4077c962b2b3ad95d55e7efe
Andien99 <170013021+Andien99@users.noreply.github.com>
2025-04-28 21:08:50 +1000

    added DOM manipulation features that allowed books to take up library div space

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Designing the modal
 const openModal = document.querySelector('#open-modal')
 const modal = document.querySelector('#modal')
+const library = document.querySelector('#library')
 const cancelBtn = document.querySelector('#cancelbtn')
-
+const submitBtn = document.querySelector('#submitbtn')
+const form = document.querySelector('#form')
 openModal.addEventListener ('click', () => { 
     console.log('works')
     modal.classList.add('open')
@@ -18,11 +20,11 @@ const myLibrary = []
 
 let NewBook = '';
 
-const submitBtn = document.querySelector('#submitbtn')
+
 
 submitBtn.addEventListener( 'click', function(event) {
-
-    
+    event.preventDefault()
+    // Gets parameters for a book based on user input on forms and adds to the mylibrary array
     NewBook = new Book(
     document.getElementById('title').value,
     document.getElementById('author').value,
@@ -30,10 +32,10 @@ submitBtn.addEventListener( 'click', function(event) {
     document.getElementById('status').value,
     crypto.randomUUID()
     )
-
     addBookToLibrary()
-    
-    event.preventDefault();
+    // resets form to default
+    form.reset()
+    modal.classList.remove('open')
 })
 
 
@@ -46,5 +48,35 @@ function Book(title, author, pagesRead, status, UUID) {
 }
 
 function addBookToLibrary () {
-    myLibrary.push(NewBook)
+    myLibrary.push(NewBook);
+    displayBook()
+}
+
+// function that loops through array and displays each book
+
+function displayBook () {
+    for (let i = 0; i < myLibrary.length; i++) {
+        if 
+        // appends the new book to the main page
+        const newBook = document.createElement('div')
+        newBook.classList.add('new')
+
+        const title = document.createElement('p')
+        title.textContent =  'Title: ' +  document.getElementById('title').value
+        newBook.appendChild(title)
+
+        const author = document.createElement('p');
+        author.textContent = 'Author:' +  document.getElementById('author').value
+        newBook.appendChild(author)
+
+        const pagesRead = document.createElement('p')
+        pagesRead.textContent = 'Pages read: ' + document.getElementById('pages-read').value
+        newBook.appendChild(pagesRead)
+
+        const condition = document.createElement('p')
+        condition.textContent =  'status: ' + document.getElementById('status').value;
+        newBook.appendChild(condition)
+
+        library.appendChild(newBook)
+    }
 }
